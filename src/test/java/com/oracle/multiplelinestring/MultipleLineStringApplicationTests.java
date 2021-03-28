2ebf387a3f30e1418c4aa73b979c71c666fca1a2
Venkatesh Thalluri <venkateshthalluri123@gmail.com>
2021-03-28 14:19:05 +0530

    initialised

---
+++ src/test/java/com/oracle/multiplelinestring/MultipleLineStringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oracle.multiplelinestring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultipleLineStringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
